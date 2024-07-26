ff7444c2b6d1e11b820771269b9077011831ccc5
Xurshid <muhammadov.xurshid@mail.ru>
2024-07-27 03:20:36 +0500

    git three
    git three

---
+++ workout-master/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Transition>
+            <Loading v-if="loading" />
+            <div class="container" v-else>
+                <AppNavbar />
+                <Nuxt />
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script>
+import AppNavbar from '../components/AppNavbar.vue';
+
+export default {
+    data() {
+        return {
+            loading: true
+        }
+    },
+    mounted() {
+        if (process.client) {
+            this.loading = false;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
